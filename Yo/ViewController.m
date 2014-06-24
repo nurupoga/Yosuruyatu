9e8fc5d5cf5830271eda9eabeeaad5aabea8917c
ohtatomotaka <ohtatomotaka@dot1x060.14-60-23.wlan.ms.dendai.ac.jp>
2014-06-24 13:28:26 +0900

    垢消せ

--- Yo/ViewController.m
+++ Yo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 #import "FHSTwitterEngine.h"
 @interface ViewController (){
-    NSArray *member;
+    NSMutableArray *member;
 }
 @end
 @implementation ViewController
@@ -17,11 +17,13 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [[FHSTwitterEngine sharedEngine]permanentlySetConsumerKey:@"Xg3ACDprWAH8loEPjMzRg" andSecret:@"9LwYDxw1iTc6D9ebHdrYCZrJP4lJhQv5uf4ueiPHvJ0"];
+    //    [[FHSTwitterEngine sharedEngine]permanentlySetConsumerKey:@"Xg3ACDprWAH8loEPjMzRg" andSecret:@"9LwYDxw1iTc6D9ebHdrYCZrJP4lJhQv5uf4ueiPHvJ0"];
     //するやつーーーー
-//    [[FHSTwitterEngine sharedEngine]permanentlySetConsumerKey:@"zhYrTjnp7IQAcBGTN6r50OWZR" andSecret:@"VPblHDqGjFd5L0ahPWnQNRiYhed5eOjzKleYkKPgRUPX26L3gg"];
+    [[FHSTwitterEngine sharedEngine]permanentlySetConsumerKey:@"zhYrTjnp7IQAcBGTN6r50OWZR" andSecret:@"VPblHDqGjFd5L0ahPWnQNRiYhed5eOjzKleYkKPgRUPX26L3gg"];
     [[FHSTwitterEngine sharedEngine]setDelegate:self];
     [[FHSTwitterEngine sharedEngine]loadAccessToken];
+    member = [[NSMutableArray alloc] initWithObjects:@"@numpad0", @"@nurupoga", nil];
+    [member addObject:@"➕"];
     
     table.separatorStyle = UITableViewCellSeparatorStyleNone;
 }
@@ -40,7 +42,6 @@
     // Dispose of any resources that can be recreated.
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    member = [[NSArray alloc] initWithObjects:@"@numpad0", @"@kir1ca", @"@mystia04", @"@a_w0_1",@"@Skycanvas2000", nil];
     return [member count];
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -57,9 +58,27 @@
 }
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSString *yomessage=[NSString stringWithFormat:@"%@ Yo",member[indexPath.row]];
-    [[FHSTwitterEngine sharedEngine] postTweet:yomessage];
-    NSLog(@"%d",indexPath.row);
+    if ([member count]==indexPath.row+1) {
+        UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Hello!" message:@"Please enter your friend name:" delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+        alert.alertViewStyle = UIAlertViewStylePlainTextInput;
+        UITextField * alertTextField = [alert textFieldAtIndex:0];
+        //alertTextField.keyboardType = UIKeyboardTypeNumberPad;
+        alertTextField.placeholder = @"@nurupoga";
+        [alert show];
+    }else{
+        NSLog(@"post");
+        NSString *yomessage=[NSString stringWithFormat:@"%@ Yo!!!",member[indexPath.row]];
+        [[FHSTwitterEngine sharedEngine] postTweet:yomessage];
+        NSLog(@"%d",indexPath.row);
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    NSLog(@"Entered: %@",[[alertView textFieldAtIndex:0] text]);
+    [member removeObject:@"➕"];
+    [member addObject:[[alertView textFieldAtIndex:0] text]];
+    [member addObject:@"➕"];
+    [table reloadData];
 }
 - (IBAction)OAuth:(id)sender {
     [self loginOAuth];
